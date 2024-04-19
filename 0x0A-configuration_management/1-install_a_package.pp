98b97b7f18d0733ce92f94fcf78d803ec1a572a4
ritaOmondi <cherylritaomondi@gmail.com>
2024-04-19 00:43:33 -0700

    installing flask using puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
-# installs the package puppet-lint
+# installing Flask using  the package puppet-lint
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
+
+#updating the werkzeug library
+#package { 'werkzeug':
+ # ensure   => '2.0.2',
+  #provider => 'pip3',
+#}
